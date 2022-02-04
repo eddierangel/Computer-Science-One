01b5fce43491c8e0a7a6e6449981ad7b6db208f6
Eddie Rangel <hacker@eddierangel.com>
2022-02-04 01:34:56 -0800

    Adding Go Program Template

---
+++ Examples/ProgramTemplate.go
@@ -0,0 +1,15 @@
+// Developer: Eddie Rangel
+// File name: ProgramTemplate.go
+// Description: Simple Program that can be used as a template
+// Copyright (c) 2022
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("\n")
+	fmt.Println("Simple Program that can be used as a template...\n")
+
+}
