a9e3d744bf494cd136875db0f5978a062d965cb0
Eddie Rangel <hacker@eddierangel.com>
2022-02-04 01:15:18 -0800

    Update Command Line Example

--- Examples/arguments.go
+++ Examples/arguments.go
@@ -15,12 +15,14 @@ func main() {
 	var element, seperator string
 
 	//fmt.Println("Filename: ", os.Args[0])
-	fmt.Println("Arguments Assignment\n")
+	fmt.Println("\n")
+	fmt.Println("Process Command Line Arguements...\n")
 
 	for index := 1; index < len(os.Args); index++ {
 		element += seperator + os.Args[index]
 		seperator = " "
 	}
 	fmt.Println(element)
+	fmt.Println("\n")
 
 }
