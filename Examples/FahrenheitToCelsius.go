4d44834a24dd756d049c1e7f1e67edaa9bbd6b5f
Eddie Rangel <hacker@eddierangel.com>
2022-02-04 01:32:18 -0800

    Adding Fahrenheit Example

---
+++ Examples/FahrenheitToCelsius.go
@@ -0,0 +1,15 @@
+// Developer: Eddie Rangel
+// File name: FahrenheitToCelsius.go
+// Description: Displays a table of Fahrenheit and Celsius values
+// Copyright (c) 2022
+
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("\n")
+	fmt.Println("Display Fahrenheit and Celsius values...\n")
+
+}
