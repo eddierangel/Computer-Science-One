dde197c7690f7834fd14389fab3436034706c642
Eddie Rangel <hacker@eddierangel.com>
2022-02-04 02:29:00 -0800

    Updating Fahrenheit Example

--- Examples/FahrenheitToCelsius.go
+++ Examples/FahrenheitToCelsius.go
@@ -12,4 +12,21 @@ func main() {
 	fmt.Println("\n")
 	fmt.Println("Display Fahrenheit and Celsius values...\n")
 
+	var fahrenheit, celsius int
+	var upper, step int
+
+	fahrenheit = 0
+	upper = 300
+	step = 20
+
+	fmt.Println("Fahrenheit\tCelsius")
+	fmt.Println("_______________________")
+
+	for index := 0; index <= upper; index += step {
+		celsius = 5 * (fahrenheit - 32) / 9
+		fmt.Printf("%d\t\t%d\n", fahrenheit, celsius)
+		fahrenheit += step
+	}
+	fmt.Println("\n")
+
 }
